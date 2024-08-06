e57d5cd00706a106eed9e798bb81d3ea31838629
tomaszbilka <bila4@op.pl>
2024-08-06 14:11:42 +0200

    feat:LC-60 add categories migration

---
+++ apps/api/src/storage/migrations/0004_create_categories.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "categories" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar,
+	"created_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"updated_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"archived_at" timestamp(3) with time zone
+);
