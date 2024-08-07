8336aeae3dd332b1b6259a262c8095ca4836af42
Sensaz <klaudiusz.wiktor1@gmail.com>
2024-08-07 13:48:28 +0200

    feat: Added the option to add video and text lessons.

--- apps/web/app/modules/Dashboard/LessonItems/Add/Text/LessonItemsAddText.layout.tsx
+++ apps/web/app/modules/Dashboard/LessonItems/Add/Text/LessonItemsAddText.layout.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLessonItems } from "~/modules/Dashboard/LessonItemsContext";
+import { DefaultValuesInterface } from "../../components/LessonItemForm";
+import { editLessonItemFormSchema } from "../../components/LessonItemForm/zodFormType";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { isObject } from "lodash-es";
+import { LessonItemForm } from "../../components/LessonItemForm/LessonItemForm";
 
 const LessonItemsAddTextLayout = () => {
-  return <div>LessonItemsAddFile.layout</div>;
+  const { setLessonItems } = useLessonItems();
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const [lessonItemForm, setLessonItemForm] = useState<DefaultValuesInterface>({
+    name: "",
+    displayName: "",
+    description: "",
+  });
+
+  const form = useForm<z.infer<typeof editLessonItemFormSchema>>({
+    resolver: zodResolver(editLessonItemFormSchema),
+    defaultValues: lessonItemForm,
+  });
+
+  const handleFileChange = (files: FileList | null) => {
+    if (isObject(files)) {
+      setVideoFile(files[0]);
+    } else setVideoFile(files);
+  };
+
+  const onSubmit: SubmitHandler<z.infer<typeof editLessonItemFormSchema>> = (
+    data
+  ) => {
+    setLessonItems((prevItems) => [
+      ...prevItems,
+      { id: new Date().getTime().toString(), ...data },
+    ]);
+  };
+
+  return (
+    <div>
+      <LessonItemForm
+        handleFileChange={handleFileChange}
+        onSubmit={onSubmit}
+        lessonItemForm={lessonItemForm}
+        videoFile={videoFile}
+        isVideoRequired={false}
+        form={form}
+      />
+    </div>
+  );
 };
 
 export default LessonItemsAddTextLayout;
--- apps/web/app/modules/Dashboard/LessonItems/Add/Video/LessonItemsAddVideo.layout.tsx
+++ apps/web/app/modules/Dashboard/LessonItems/Add/Video/LessonItemsAddVideo.layout.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { useState } from "react";
+import { useLessonItems } from "~/modules/Dashboard/LessonItemsContext";
+import { DefaultValuesInterface } from "../../components/LessonItemForm";
+import { editLessonItemFormSchema } from "../../components/LessonItemForm/zodFormType";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { isObject } from "lodash-es";
+import { LessonItemForm } from "../../components/LessonItemForm/LessonItemForm";
 
 const LessonItemsAddVideoLayout = () => {
-  return <div>LessonItemsAddVideoLayout</div>;
+  const { setLessonItems } = useLessonItems();
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const [lessonItemForm, setLessonItemForm] = useState<DefaultValuesInterface>({
+    name: "",
+    displayName: "",
+    description: "",
+    video: null,
+  });
+
+  const form = useForm<z.infer<typeof editLessonItemFormSchema>>({
+    resolver: zodResolver(editLessonItemFormSchema),
+    defaultValues: lessonItemForm,
+  });
+
+  const handleFileChange = (files: FileList | null) => {
+    if (isObject(files)) {
+      setVideoFile(files[0]);
+    } else setVideoFile(files);
+  };
+
+  const onSubmit: SubmitHandler<z.infer<typeof editLessonItemFormSchema>> = (
+    data
+  ) => {
+    if (data.video) {
+      if (data.video instanceof FileList) {
+        setVideoFile(data.video[0]);
+      } else {
+        setVideoFile(data.video);
+      }
+    } else {
+      console.log("video is required");
+    }
+
+    setLessonItems((prevItems) => [
+      ...prevItems,
+      { id: new Date().getTime().toString(), ...data },
+    ]);
+  };
+
+  return (
+    <div>
+      <LessonItemForm
+        handleFileChange={handleFileChange}
+        onSubmit={onSubmit}
+        lessonItemForm={lessonItemForm}
+        videoFile={videoFile}
+        isVideoRequired={true}
+        form={form}
+      />
+    </div>
+  );
 };
 
 export default LessonItemsAddVideoLayout;
