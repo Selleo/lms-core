60a0e462c831c0604a3c12f8d8265644f36e06d0
Sensaz <klaudiusz.wiktor1@gmail.com>
2024-08-12 13:23:57 +0200

    ref: Create generic reusable FormTextarea component

---
+++ apps/web/app/components/Form/FormTextarea.tsx
@@ -0,0 +1,39 @@
+import { Textarea } from "~/components/ui/textarea";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Control } from "react-hook-form";
+import { z } from "zod";
+import { editFormSchema } from "../zodFormType.js";
+
+interface FormTextareaInterface {
+  control: Control<z.infer<typeof editFormSchema>>;
+  name: "title" | "status" | "description" | "video";
+  label: string;
+  placeholder: string;
+}
+
+export const FormTextarea = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: FormTextareaInterface) => (
+  <FormField
+    control={control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel>{label}</FormLabel>
+        <FormControl>
+          <Textarea placeholder={placeholder} {...field} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
