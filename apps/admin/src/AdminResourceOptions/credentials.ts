124c112c2b97db8a8cf0fbfcd3b13a2f1e6659ac
Jakub Wilk <jwilk436@gmail.com>
2024-08-19 11:18:34 +0200

    feat: add basic adminjs config
    --wip-- [skip ci]
    
    refactor: update environment variable handling

---
+++ apps/admin/src/AdminResourceOptions/credentials.ts
@@ -0,0 +1,12 @@
+export const credentialsConfigOptions = {
+  options: {
+    navigation: false,
+    actions: {
+      new: { isAccessible: false },
+      edit: { isAccessible: false },
+      delete: { isAccessible: false },
+      list: { isAccessible: false },
+      show: { isAccessible: false },
+    },
+  },
+};
