797b8b0e1ce4b83f49f8eef557e1a361a74a99bf
tomaszbilka <bila4@op.pl>
2024-08-22 11:45:17 +0200

    feat: add archived categories migration

---
+++ apps/api/src/storage/migrations/0009_update_name_and_type_of_archive_in_categories.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "categories" RENAME COLUMN "archived_at" TO "archived_temp";
+ALTER TABLE "categories" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
+UPDATE "categories" SET "archived" = ("archived_temp" IS NOT NULL);
+ALTER TABLE "categories" DROP COLUMN "archived_temp";
