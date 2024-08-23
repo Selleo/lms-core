112678e5674b8b86da739af9b6ef1fc3acd161d0
Jakub Wilk <jwilk436@gmail.com>
2024-08-23 20:28:18 +0200

    --wip-- [skip ci]

--- apps/admin/src/componetns/helpers/CustomInput.tsx
+++ apps/admin/src/componetns/helpers/CustomInput.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { BasePropertyProps } from "adminjs";
-// import {
-//   Input,
-//   xd,
-// } from "../../../../../packages/ui/src/components/ui/input.js";
+
 import { Input } from "@repo/ui";
 
 interface CustomSelectComponentProps extends BasePropertyProps {
@@ -12,34 +9,20 @@ interface CustomSelectComponentProps extends BasePropertyProps {
 
 const CustomInputComponent = (
   {
-    // onChange,
     // property,
     // record,
+    // onChange,
   }: CustomSelectComponentProps,
 ) => {
   // const { name } = property;
   // const value = record?.params[name] || "";
-  // console.log({ xd });
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
-  console.log("DUPAAAAAAAAAAA");
 
   return (
     <>
-      {/* <Input
-        onChange={(e: any) => onChange(name, e.target.value)}
-        value={value}
-      /> */}
-      xd
-      <p>DUPAAAAAAAAAAA333</p>
-      {/* {xd} */}
+      <Input
+      // onChange={(e: any) => onChange(name, e.target.value)}
+      // value={value}
+      />
     </>
   );
 };
