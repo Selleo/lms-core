1d2e9e27830a3dacfd524b603765d16339b153e0
Jakub Wilk <jwilk436@gmail.com>
2024-09-02 21:47:06 +0200

    chore: updated selects with options across admin

--- apps/admin/src/AdminResourceOptions/questions.ts
+++ apps/admin/src/AdminResourceOptions/questions.ts
@@ -1,8 +1,26 @@
 import { ResourceOptions } from "adminjs";
+import { stateOptions } from "./common/consts/selectOptions/stateOptions.js";
 
 export const questionsConfigOptions: ResourceOptions = {
   parent: "lesson-items",
   properties: {
+    question_body: {
+      type: "richtext",
+    },
+    solution_explanation: {
+      type: "richtext",
+    },
+    question_type: {
+      availableValues: [
+        { value: "open_answer", label: "Open Answer" },
+        { value: "single_choice", label: "Single Choice" },
+        { value: "multiple_choice", label: "Multiple Choice" },
+        { value: "fill_in_the_blanks", label: "Fill in the blanks" },
+      ],
+    },
+    status: {
+      availableValues: [...stateOptions],
+    },
     created_at: {
       position: 7,
       isVisible: {
