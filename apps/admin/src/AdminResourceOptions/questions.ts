f684101ee7215e206122159308d5a23574ab306c
Jakub Wilk <jwilk436@gmail.com>
2024-09-02 20:40:30 +0200

    feat: add created_at and updated_at properties

--- apps/admin/src/AdminResourceOptions/questions.ts
+++ apps/admin/src/AdminResourceOptions/questions.ts
@@ -2,4 +2,25 @@ import { ResourceOptions } from "adminjs";
 
 export const questionsConfigOptions: ResourceOptions = {
   parent: "lesson-items",
+  properties: {
+    created_at: {
+      position: 7,
+      isVisible: {
+        edit: false,
+        list: true,
+        show: true,
+        filter: false,
+      },
+      isSortable: false,
+    },
+    updated_at: {
+      position: 8,
+      isVisible: {
+        edit: false,
+        list: false,
+        show: true,
+        filter: false,
+      },
+    },
+  },
 };
