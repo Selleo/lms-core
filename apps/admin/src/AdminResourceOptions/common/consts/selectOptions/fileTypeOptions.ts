cbf579eb90dd8dad831f91d6b19eea4ae5816725
tomaszbilka <bila4@op.pl>
2024-09-03 12:05:23 +0200

    feat: LC-22 add archiving to files

---
+++ apps/admin/src/AdminResourceOptions/common/consts/selectOptions/fileTypeOptions.ts
@@ -0,0 +1,6 @@
+export const fileTypeOptions = [
+  { value: "presentation", label: "Presentation" },
+  { value: "external_presentation", label: "External Presentation" },
+  { value: "video", label: "Video" },
+  { value: "external_video", label: "External Video" },
+];
