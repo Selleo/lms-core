99954430fed82b2c9a9c1ee015095b35f0179658
tomaszbilka <bila4@op.pl>
2024-09-03 12:56:07 +0200

    feat: LC-22 add archiving to questions

---
+++ apps/admin/src/AdminResourceOptions/common/consts/selectOptions/questionValueOptions.ts
@@ -0,0 +1,6 @@
+export const questionValueOptions = [
+  { value: "open_answer", label: "Open Answer" },
+  { value: "single_choice", label: "Single Choice" },
+  { value: "multiple_choice", label: "Multiple Choice" },
+  { value: "fill_in_the_blanks", label: "Fill in the blanks" },
+];
