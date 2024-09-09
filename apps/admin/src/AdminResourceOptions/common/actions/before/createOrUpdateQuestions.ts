09253c5b21b79d6cff6c25bc16f333913fe12227
tomaszbilka <bila4@op.pl>
2024-09-09 15:07:47 +0200

    feat: LC-159 add questions form

---
+++ apps/admin/src/AdminResourceOptions/common/actions/before/createOrUpdateQuestions.ts
@@ -0,0 +1,48 @@
+import { type ActionRequest, type Before, ValidationError } from "adminjs";
+
+export const beforeCreateOrUpdateQuestions: Before = async (
+  request: ActionRequest,
+) => {
+  const { payload = {}, method } = request;
+
+  if (method !== "post") return request;
+
+  const errors: { [key: string]: { message: string } } = {};
+  const { question_type, question_body, solution_explanation, state } = payload;
+
+  if (!question_type) {
+    errors.question_type = { message: "Please provide type of the question" };
+  }
+
+  if (!question_body) {
+    errors.question_body = { message: "Please provide question body" };
+  }
+
+  if (question_body?.length > 2000) {
+    errors.question_body = {
+      message: `Question body must be no more than 2000 characters.`,
+    };
+  }
+
+  if (!solution_explanation) {
+    errors.solution_explanation = {
+      message: "Please provide solution explanation",
+    };
+  }
+
+  if (solution_explanation?.length > 2000) {
+    errors.solution_explanation = {
+      message: `Solution explanation must be no more than 2000 characters.`,
+    };
+  }
+
+  if (!state) {
+    errors.state = { message: "Please provide a state" };
+  }
+
+  if (Object.keys(errors).length) {
+    throw new ValidationError(errors);
+  }
+
+  return request;
+};
