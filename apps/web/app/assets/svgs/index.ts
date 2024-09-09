8d6ff1615826918a6d0e529b787ca5a6ca1696f0
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-09-10 00:27:55 +0200

    feat: add new icons

---
+++ apps/web/app/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { default as CaretDown } from "./carret-down.svg?react";
+export { default as SelleoLogo } from "./selleo-logo.svg?react";
+export { default as Directory } from "./directory.svg?react";
+export { default as Dashboard } from "./dashboard.svg?react";
