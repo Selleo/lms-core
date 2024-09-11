ecbd72e072f6431f357a5d3993ed20e14709d7e0
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-09-11 14:57:06 +0200

    Feat/LC-164 Custom Dashboard Theme (#79)
    * feat: apply figma styleguide to our config
    
    * refactor: pdate navigation menu to match figma layout
    
    * fix: LC-164 add styles to navigation
    
    ---------
    
    Co-authored-by: tomaszbilka <bila4@op.pl>

--- apps/web/app/modules/Dashboard/DashboardNavigation/MenuItem.tsx
+++ apps/web/app/modules/Dashboard/DashboardNavigation/MenuItem.tsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 import { MenuItemLink } from "./MenuItemLink";
+import { Icon } from "~/components/Icon";
 import type { MenuItemType } from "./DashboardNavigation";
 
 type MenuItemProps = {
@@ -9,13 +10,18 @@ type MenuItemProps = {
 export const MenuItem = memo(({ item }: MenuItemProps) => {
   if ("children" in item) {
     return (
-      <li className="w-full mt-4 max-w-[268px]">
-        <span className="block font-medium text-gray-600 capitalize">
-          {item.label}
-        </span>
+      <li className="w-full my-2 h-8 p-2 mt-3">
+        <div className="flex items-center border-b border-b-primary-200">
+          <Icon name={"Directory"} className="w-4 h-4 mr-2 text-neutral-900" />
+          <span className="block subtle text-neutral-900 capitalize  p-2">
+            {item.label}
+          </span>
+        </div>
         <ul>
           {item.children.map((child) => (
-            <MenuItem key={child.label} item={child} />
+            <div key={child.label} className="ml-4">
+              <MenuItem item={child} />
+            </div>
           ))}
         </ul>
       </li>
@@ -23,7 +29,7 @@ export const MenuItem = memo(({ item }: MenuItemProps) => {
   }
 
   return (
-    <li className="h-8 w-full">
+    <li className="my-2 h-8 w-full">
       <MenuItemLink {...item} />
     </li>
   );
