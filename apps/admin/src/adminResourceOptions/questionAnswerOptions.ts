c36c4d968eb4eb89e0566d784bc58d5bd573a0db
tomaszbilka <bila4@op.pl>
2024-09-17 10:57:24 +0200

    feat: LC-185 add redirect to parent question

--- apps/admin/src/adminResourceOptions/questionAnswerOptions.ts
+++ apps/admin/src/adminResourceOptions/questionAnswerOptions.ts
@@ -1,21 +1,35 @@
 import { ResourceOptions } from "adminjs";
 import { Components } from "../components/index.js";
 import { beforeCreateOrUpdateQuestionAnswer } from "./common/actions/before/beforeCreateOrUpdateQuestionAnswer.js";
+import { createQuestionAnswer } from "./common/actions/after/createQuestionAnswer.js";
 
 export const questionAnswerConfigOptions: ResourceOptions = {
   actions: {
     new: {
+      after: [createQuestionAnswer],
       before: [beforeCreateOrUpdateQuestionAnswer],
     },
     edit: {
+      after: [createQuestionAnswer],
       before: [beforeCreateOrUpdateQuestionAnswer],
     },
   },
   navigation: false,
   showProperties: ["question_id", "option_text", "is_correct", "position"],
-  editProperties: ["question_id", "option_text", "is_correct", "position"],
+  editProperties: [
+    "question_id",
+    "option_text",
+    "is_correct",
+    "blankInfo",
+    "position",
+  ],
   listProperties: ["question_id", "option_text", "is_correct", "position"],
   properties: {
+    blankInfo: {
+      components: {
+        edit: Components.BlankInfo,
+      },
+    },
     question_id: {
       isRequired: false,
       components: {
