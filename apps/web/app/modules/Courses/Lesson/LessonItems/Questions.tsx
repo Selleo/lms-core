789d6c8afa4f3885d49bd5f0c4683b7c3b4f44cb
tomaszbilka <bila4@op.pl>
2024-09-24 13:11:55 +0200

    feat: LC-224 add open question type

--- apps/web/app/modules/Courses/Lesson/LessonItems/Questions.tsx
+++ apps/web/app/modules/Courses/Lesson/LessonItems/Questions.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Card } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
 import { cn } from "~/lib/utils";
 
 type TProps = {
@@ -19,7 +20,9 @@ type TProps = {
 
 export default function Questions({ content }: TProps) {
   const [seletedOption, setSeletedOption] = useState<string[]>([]);
+  const [openQuestion, setOpenQuestion] = useState("");
   const isSingleQuestion = content.questionType === "single_choice";
+  const isOpenAnswer = content.questionType === "open_answer";
 
   const handleClcik = (id: string) => {
     if (isSingleQuestion) {
@@ -40,33 +43,44 @@ export default function Questions({ content }: TProps) {
         className="h6 text-neutral-950"
         dangerouslySetInnerHTML={{ __html: content.questionBody }}
       />
-      <div className="body-base-md text-neutral-900">{`Type: ${isSingleQuestion ? "Single" : "Multiple"} select question.`}</div>
+      <div className="body-base-md text-neutral-900">
+        {isOpenAnswer
+          ? `Instruction: Provide a brief response.`
+          : `Type: ${isSingleQuestion ? "Single" : "Multiple"} select question.`}
+      </div>
       <div className="flex flex-col gap-4 mt-4">
-        {content.questionAnswers.map((answer) => (
-          <button
-            onClick={() => handleClcik(answer.id)}
-            key={answer.id}
-            className="flex items-center space-x-3 border border-primary-200 rounded-lg py-3 px-4"
-          >
-            <Input
-              className={cn("w-4 h-4 border-red-500", {
-                "radio-square": !isSingleQuestion,
-              })}
-              checked={seletedOption.includes(answer.id)}
-              id={answer.id}
-              readOnly
-              type="radio"
-              value={answer.id}
-            />
-            <Label
-              className="body-base text-neutral-950"
-              htmlFor={answer.id}
-              onClick={(e) => e.stopPropagation()}
+        {isOpenAnswer ? (
+          <Textarea
+            value={openQuestion}
+            onChange={(e) => setOpenQuestion(e.target.value)}
+          />
+        ) : (
+          content.questionAnswers.map((answer) => (
+            <button
+              onClick={() => handleClcik(answer.id)}
+              key={answer.id}
+              className="flex items-center space-x-3 border border-primary-200 rounded-lg py-3 px-4"
             >
-              {answer.optionText}
-            </Label>
-          </button>
-        ))}
+              <Input
+                className={cn("w-4 h-4 border-red-500", {
+                  "radio-square": !isSingleQuestion,
+                })}
+                checked={seletedOption.includes(answer.id)}
+                id={answer.id}
+                readOnly
+                type="radio"
+                value={answer.id}
+              />
+              <Label
+                className="body-base text-neutral-950"
+                htmlFor={answer.id}
+                onClick={(e) => e.stopPropagation()}
+              >
+                {answer.optionText}
+              </Label>
+            </button>
+          ))
+        )}
       </div>
     </Card>
   );
