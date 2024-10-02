770332c2c7c29324d69b22daebb172cce0a0c7d1
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-10-02 11:45:14 +0200

    refactor: extract all courses validation to separate file

---
+++ apps/api/src/courses/api/validations.ts
@@ -0,0 +1,30 @@
+import { paginatedResponse } from "src/common";
+import { allCoursesSchema } from "src/courses/schemas/course.schema";
+import { Type } from "@sinclair/typebox";
+import { sortCourseFieldsOptions } from "src/courses/schemas/courseQuery";
+
+export const allCoursesValidation = {
+  response: paginatedResponse(allCoursesSchema),
+  request: [
+    { type: "query" as const, name: "title", schema: Type.String() },
+    { type: "query" as const, name: "category", schema: Type.String() },
+    { type: "query" as const, name: "author", schema: Type.String() },
+    {
+      type: "query" as const,
+      name: "creationDateRange[0]",
+      schema: Type.String(),
+    },
+    {
+      type: "query" as const,
+      name: "creationDateRange[1]",
+      schema: Type.String(),
+    },
+    {
+      type: "query" as const,
+      name: "page",
+      schema: Type.Number({ minimum: 1 }),
+    },
+    { type: "query" as const, name: "perPage", schema: Type.Number() },
+    { type: "query" as const, name: "sort", schema: sortCourseFieldsOptions },
+  ],
+};
