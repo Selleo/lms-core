18b0b2c22adbea03c730eb07148569d142060ef1
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-10-04 10:06:37 +0200

    refactor: remove navigation from settings view

--- apps/web/app/modules/Dashboard/Settings/Settings.layout.tsx
+++ apps/web/app/modules/Dashboard/Settings/Settings.layout.tsx
@@ -1,38 +1,8 @@
-import { Link, Outlet, useLocation } from "@remix-run/react";
-import { replace } from "lodash-es";
-import { cn } from "~/lib/utils";
+import { Outlet } from "@remix-run/react";
 
 export default function SettingsLayout() {
-  const { hash } = useLocation();
-  const currentTab = replace(hash, "#", "") || "user-info";
-
-  const navigationItems = [
-    {
-      id: "user-info",
-      title: "User Info",
-    },
-    {
-      id: "change-password",
-      title: "Change Password",
-    },
-  ];
-
   return (
-    <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr] p-4">
-      <nav className="grid gap-4 text-sm text-muted-foreground">
-        <h1 className="text-3xl font-semibold text-primary">Settings</h1>
-        {navigationItems.map((item) => (
-          <Link
-            key={item.id}
-            to={`#${item.id}`}
-            className={cn({
-              "text-primary": currentTab === item.id,
-            })}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </nav>
+    <div className="flex flex-col gap-6 p-4">
       <Outlet />
     </div>
   );
