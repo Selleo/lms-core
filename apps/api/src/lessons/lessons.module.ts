8049b71e3c0412904108d25de52086d3ced4e150
wielopolski <wielopolskim@gmail.com>
2024-10-31 16:56:38 +0100

    feat: refactor lessonItem  service

--- apps/api/src/lessons/lessons.module.ts
+++ apps/api/src/lessons/lessons.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { LessonsService } from "./lessons.service";
 import { LessonsController } from "./api/lessons.controller";
 import { S3Service } from "src/file/s3.service";
-import { LessonItemsService } from "./lessonItems.service";
+import { AdminLessonItemsService } from "./adminLessonItems.service";
 import { LessonsRepository } from "./repositories/lessons.repository";
 import { AdminLessonsRepository } from "./repositories/adminLessons.repository";
 import { AdminLessonsService } from "./adminLessons.service";
+import { AdminLessonItemsRepository } from "./repositories/adminLessonItems.repository";
 
 @Module({
   imports: [],
@@ -13,10 +14,11 @@ import { AdminLessonsService } from "./adminLessons.service";
   providers: [
     LessonsService,
     AdminLessonsService,
-    LessonItemsService,
+    AdminLessonItemsService,
     S3Service,
     LessonsRepository,
     AdminLessonsRepository,
+    AdminLessonItemsRepository,
   ],
   exports: [LessonsRepository],
 })
