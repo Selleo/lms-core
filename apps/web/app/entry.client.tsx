081a7f59511b86e1bfa57d4cd183f2b7688182bc
Jakub Wilk <jwilk436@gmail.com>
2024-11-24 23:25:06 +0100

    feat: integrate Sentry for error tracking and performance monitoring in web app

--- apps/web/app/entry.client.tsx
+++ apps/web/app/entry.client.tsx
@@ -3,20 +3,54 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
-
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import {
+  RemixBrowser,
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "@remix-run/react";
+import * as Sentry from "@sentry/react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 import { Providers } from "./modules/Global/Providers";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    // eslint-disable-next-line import/namespace
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+  environment: import.meta.env.MODE,
+  beforeSend(event) {
+    if (import.meta.env.DEV) {
+      return null;
+    }
+    return event;
+  },
+});
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <Providers>
-        <RemixBrowser />
-      </Providers>
+      <Sentry.ErrorBoundary fallback={<div>An error occurred</div>}>
+        <Providers>
+          <RemixBrowser />
+        </Providers>
+      </Sentry.ErrorBoundary>
     </StrictMode>,
   );
 });
