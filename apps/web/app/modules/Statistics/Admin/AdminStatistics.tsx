67bba7a01fde2c259ab21b9c3dd1fb6a1f955e2e
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-12-02 14:59:23 +0100

    feat: course completion percentage chart

--- apps/web/app/modules/Statistics/Admin/AdminStatistics.tsx
+++ apps/web/app/modules/Statistics/Admin/AdminStatistics.tsx
@@ -1,13 +1,47 @@
+import { useMemo } from "react";
+
 import { useCurrentUser, useTeacherStatistics } from "~/api/queries";
 import { Gravatar } from "~/components/Gravatar";
 import { PageWrapper } from "~/components/PageWrapper";
 import { Avatar } from "~/components/ui/avatar";
 
-import { FiveMostPopularCoursesChart } from "./components";
+import { CourseCompletionPercentageChart, FiveMostPopularCoursesChart } from "./components";
+
+import type { ChartConfig } from "~/components/ui/chart";
 
 export const AdminStatistics = () => {
   const { data: user } = useCurrentUser();
   const { data: statistics, isLoading } = useTeacherStatistics();
+
+  const totalCoursesCompletion = statistics?.totalCoursesCompletionStats.totalCoursesCompletion;
+  const totalCourses = statistics?.totalCoursesCompletionStats.totalCourses;
+
+  const coursesCompletionChartConfig = {
+    completed: {
+      label: `Completed - ${totalCoursesCompletion}`,
+      color: "var(--primary-700)",
+    },
+    notCompleted: {
+      label: `Enrolled - ${totalCourses}`,
+      color: "var(--primary-300)",
+    },
+  } satisfies ChartConfig;
+
+  const coursesCompletionChartData = useMemo(
+    () => [
+      {
+        state: "Completed",
+        percentage: totalCoursesCompletion,
+        fill: "var(--primary-700)",
+      },
+      {
+        state: "Enrolled",
+        percentage: totalCourses,
+        fill: "var(--primary-300)",
+      },
+    ],
+    [totalCoursesCompletion, totalCourses],
+  );
 
   return (
     <PageWrapper className="flex flex-col gap-y-6 xl:gap-y-8 xl:!h-full">
@@ -22,10 +56,16 @@ export const AdminStatistics = () => {
           data={statistics?.fiveMostPopularCourses}
           isLoading={isLoading}
         />
+        <CourseCompletionPercentageChart
+          isLoading={isLoading}
+          label={`${statistics?.totalCoursesCompletionStats.completionPercentage}`}
+          title="Course Completition Percentage"
+          chartConfig={coursesCompletionChartConfig}
+          chartData={coursesCompletionChartData}
+        />
         <div className="p-6 bg-white rounded-lg drop-shadow-card md:col-span-1 xl:col-span-1 w-full h-[400px] xl:h-full"></div>
-        <div className="p-6 bg-white rounded-lg drop-shadow-card md:col-span-1 xl:col-span-1 w-full h-[400px] xl:h-full"></div>
+        <div className="p-6 bg-white rounded-lg drop-shadow-card md:col-span-1 h-[400px] xl:col-span-2 w-full md:h-full"></div>
         <div className="p-6 bg-white rounded-lg drop-shadow-card md:col-span-2 xl:col-span-2 w-full h-[447px] xl:h-full"></div>
-        <div className="p-6 bg-white rounded-lg drop-shadow-card md:col-span-2 xl:col-span-2 w-full h-[400px] xl:h-full"></div>
       </div>
     </PageWrapper>
   );
