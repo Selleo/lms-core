b7fdc3b1d56a36e3752cbbb67a299dff15e48834
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-12-05 12:22:01 +0100

    refactor: small bugfixes

---
+++ apps/web/app/modules/Teacher/TeacherPageBreadcrumbs.tsx
@@ -0,0 +1,27 @@
+import {
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+
+type TutorPageBreadcrumbsProps = {
+  id: string;
+  username: string;
+};
+
+export const TeacherPageBreadcrumbs = ({ id, username }: TutorPageBreadcrumbsProps) => {
+  return (
+    <div className="bg-primary-50 mb-4">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/`}>Dashboard</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem className="text-neutral-950">
+          <BreadcrumbLink href={`/teachers/${id}`}>{username}</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </div>
+  );
+};
