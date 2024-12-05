1b0924e20cda303344299b0a7ed6d9ac27e17832
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-12-05 11:28:23 +0100

    feat: current user store

--- apps/web/app/components/Navigation/NavigationFooter.tsx
+++ apps/web/app/components/Navigation/NavigationFooter.tsx
@@ -3,13 +3,13 @@ import { capitalize } from "lodash-es";
 import { type Dispatch, type SetStateAction, startTransition } from "react";
 
 import { useLogoutUser } from "~/api/mutations";
-import { useCurrentUser } from "~/api/queries";
 import { Gravatar } from "~/components/Gravatar";
 import { Icon } from "~/components/Icon";
 import { Avatar } from "~/components/ui/avatar";
 import { Separator } from "~/components/ui/separator";
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/ui/tooltip";
 import { cn } from "~/lib/utils";
+import { useCurrentUserStore } from "~/modules/common/store/useCurrentUserStore";
 
 type NavigationFooterProps = {
   isAdmin: boolean;
@@ -24,7 +24,7 @@ export function NavigationFooter({
   role,
   setIsMobileNavOpen,
 }: NavigationFooterProps) {
-  const { data: user } = useCurrentUser();
+  const { currentUser } = useCurrentUserStore();
   const { mutate: logout } = useLogoutUser();
   const { pathname } = useLocation();
   const isAdminRoute = pathname.startsWith("/admin");
@@ -32,9 +32,9 @@ export function NavigationFooter({
   const isAllowed = isAdmin || isTeacher;
 
   return (
-    <menu className="grid grid-cols-2 gap-2 md:gap-4 2xl:gap-2 md:grid-cols-6 w-full 2xl:flex 2xl:flex-col 2xl:self-end">
-      <li className="2xl:hidden col-span-2 md:col-span-6">
-        <Separator className="3xl:my-2 2xl:h-[1px] bg-primary-200" />
+    <menu className="grid w-full grid-cols-2 gap-2 md:grid-cols-6 md:gap-4 2xl:flex 2xl:flex-col 2xl:gap-2 2xl:self-end">
+      <li className="col-span-2 md:col-span-6 2xl:hidden">
+        <Separator className="3xl:my-2 bg-primary-200 2xl:h-[1px]" />
       </li>
       {isAllowed && (
         <li className="col-span-2 md:col-span-3">
@@ -42,18 +42,18 @@ export function NavigationFooter({
             <TooltipTrigger className="w-full">
               <NavLink
                 onClick={() => setIsMobileNavOpen(false)}
-                className="flex gap-x-3 items-center py-3.5 px-4 rounded-lg w-full 2xl:p-2 bg-white text-neutral-900"
+                className="flex w-full items-center gap-x-3 rounded-lg bg-white px-4 py-3.5 text-neutral-900 2xl:p-2"
                 to={isAdminRoute ? "/" : "/admin/courses"}
               >
                 <Icon name={isAdminRoute ? "Dashboard" : "Admin"} className="size-6 pl-[2px]" />
-                <span className="2xl:sr-only 3xl:not-sr-only">
+                <span className="3xl:not-sr-only 2xl:sr-only">
                   {isAdminRoute ? "Dashboard" : <>{capitalize(role)} panel</>}{" "}
                 </span>
               </NavLink>
             </TooltipTrigger>
             <TooltipContent
               side="right"
-              className="hidden 2xl:block 2xl:capitalize 3xl:hidden 2xl:bg-neutral-950 2xl:text-white"
+              className="3xl:hidden hidden 2xl:block 2xl:bg-neutral-950 2xl:capitalize 2xl:text-white"
             >
               {isAdminRoute ? "Dashboard" : <>{capitalize(role)} panel</>}
             </TooltipContent>
@@ -67,19 +67,19 @@ export function NavigationFooter({
               onClick={() => setIsMobileNavOpen(false)}
               to="/settings"
               className={({ isActive }) =>
-                cn("flex gap-x-3 items-center py-3.5 px-4 rounded-lg w-full 2xl:p-2", {
+                cn("flex w-full items-center gap-x-3 rounded-lg px-4 py-3.5 2xl:p-2", {
                   "bg-primary-700 text-white": isActive,
                   "bg-white text-neutral-900": !isActive,
                 })
               }
             >
               <Icon name="Settings" className="size-6" />
-              <span className="2xl:sr-only 3xl:not-sr-only">Settings</span>
+              <span className="3xl:not-sr-only 2xl:sr-only">Settings</span>
             </NavLink>
           </TooltipTrigger>
           <TooltipContent
             side="right"
-            className="hidden 2xl:block 2xl:capitalize 3xl:hidden 2xl:bg-neutral-950 2xl:text-white"
+            className="3xl:hidden hidden 2xl:block 2xl:bg-neutral-950 2xl:capitalize 2xl:text-white"
           >
             Settings
           </TooltipContent>
@@ -88,39 +88,39 @@ export function NavigationFooter({
       <li className={cn("md:col-span-3", { "sr-only": isAdminRoute || !isAllowed })}>
         <NavLink
           onClick={() => setIsMobileNavOpen(false)}
-          to={`/teachers/${user?.id}`}
+          to={`/teachers/${currentUser?.id}`}
           className={({ isActive }) =>
-            cn("flex gap-x-3 items-center py-3.5 px-4 rounded-lg w-full 2xl:p-2", {
+            cn("flex w-full items-center gap-x-3 rounded-lg px-4 py-3.5 2xl:p-2", {
               "bg-primary-700 text-white": isActive,
               "bg-white text-neutral-900": !isActive,
             })
           }
         >
           <Icon name="User" className="size-6" />
-          <span className="2xl:sr-only 3xl:not-sr-only">Profile</span>
+          <span className="3xl:not-sr-only 2xl:sr-only">Profile</span>
         </NavLink>
       </li>
       <li className="sr-only 2xl:not-sr-only">
-        <div className="flex flex-col items-center 3xl:gap-x-2 justify-center 3xl:flex-row rounded-lg px-1 py-4 bg-neutral-50 3xl:p-3 3xl:mt-4">
-          <Avatar className="size-8 3xl:size-10">
-            <Gravatar email={user?.email} size={32} />
+        <div className="3xl:gap-x-2 3xl:flex-row 3xl:p-3 3xl:mt-4 flex flex-col items-center justify-center rounded-lg bg-neutral-50 px-1 py-4">
+          <Avatar className="3xl:size-10 size-8">
+            <Gravatar email={currentUser?.email} size={32} />
           </Avatar>
-          <hgroup className="flex flex-col justify-center sr-only 3xl:not-sr-only">
-            <h2 className="text-neutral-950 body-sm-md">
-              {user?.firstName} {user?.lastName}
+          <hgroup className="3xl:not-sr-only sr-only flex flex-col justify-center">
+            <h2 className="body-sm-md text-neutral-950">
+              {currentUser?.firstName} {currentUser?.lastName}
             </h2>
-            <p className="details text-neutral-600">{user?.email}</p>
+            <p className="details text-neutral-600">{currentUser?.email}</p>
           </hgroup>
         </div>
       </li>
       <li className="hidden 2xl:block">
-        <Separator className="2xl:h-[1px] bg-primary-200" />
+        <Separator className="bg-primary-200 2xl:h-[1px]" />
       </li>
       <li
         className={cn({
-          "md:col-span-6 col-span-1": isAllowed && isAdminRoute,
-          "md:col-span-3 col-span-2": isAllowed && !isAdminRoute,
-          "md:col-span-6 col-span-2": !isAllowed,
+          "col-span-1 md:col-span-6": isAllowed && isAdminRoute,
+          "col-span-2 md:col-span-3": isAllowed && !isAdminRoute,
+          "col-span-2 md:col-span-6": !isAllowed,
         })}
       >
         <Tooltip>
@@ -131,15 +131,15 @@ export function NavigationFooter({
                   logout();
                 });
               }}
-              className="flex gap-x-3 items-center py-3.5 px-4 rounded-lg w-full 2xl:p-2 bg-white text-neutral-900"
+              className="flex w-full items-center gap-x-3 rounded-lg bg-white px-4 py-3.5 text-neutral-900 2xl:p-2"
             >
               <Icon name="Logout" className="size-6" />
-              <span className="2xl:sr-only 3xl:not-sr-only">Logout</span>
+              <span className="3xl:not-sr-only 2xl:sr-only">Logout</span>
             </button>
           </TooltipTrigger>
           <TooltipContent
             side="right"
-            className="hidden 2xl:block 2xl:capitalize 3xl:hidden 2xl:bg-neutral-950 2xl:text-white"
+            className="3xl:hidden hidden 2xl:block 2xl:bg-neutral-950 2xl:capitalize 2xl:text-white"
           >
             Logout
           </TooltipContent>
