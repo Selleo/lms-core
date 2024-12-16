7dce85b28b42dc7ed67839fccd09a07642a78b71
Piotr Pająk <piotrpajak.programista@gmail.com>
2024-12-16 14:44:41 +0100

    refactor: change location of lesson type svgs

---
+++ apps/web/app/assets/svgs/lesson-types/index.ts
@@ -0,0 +1,4 @@
+export { default as Presentation } from "./presentation.svg?react";
+export { default as Quiz } from "./quiz.svg?react";
+export { default as Video } from "./video.svg?react";
+export { default as Text } from "./text.svg?react";
