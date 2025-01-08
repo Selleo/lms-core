8ed88d0afd3941d5974b1d54216347f80293a4cd
Piotr Pająk <piotrpajak.programista@gmail.com>
2025-01-08 14:11:36 +0100

    feat: photo question single choice question component

---
+++ apps/web/app/modules/Courses/Lesson/Question/PhotoQuestionSingleChoice/PhotoQuestionSingleChoice.tsx
@@ -0,0 +1,39 @@
+import { useUserRole } from "~/hooks/useUserRole";
+
+import { QuestionCard } from "../QuestionCard";
+import { SingleChoiceOptionList } from "../SingleChoice/SingleChoiceOptionList";
+
+import type { QuizQuestion } from "../types";
+
+type PhotoQuestionSingleChoiceProps = {
+  question: QuizQuestion;
+  isQuizCompleted?: boolean;
+};
+
+export const PhotoQuestionSingleChoice = ({
+  question,
+  isQuizCompleted = false,
+}: PhotoQuestionSingleChoiceProps) => {
+  const { isAdmin } = useUserRole();
+
+  return (
+    <QuestionCard
+      title={question.title ?? ""}
+      questionType="Single select question."
+      questionNumber={question.displayOrder ?? 0}
+    >
+      <img
+        src={question.photoS3Key || "https://placehold.co/960x620/png"}
+        alt=""
+        className="w-full h-auto max-w-[960px] rounded-lg"
+      />
+      <SingleChoiceOptionList
+        options={question.options || []}
+        questionId={question.id}
+        isAdmin={isAdmin}
+        isQuizCompleted={isQuizCompleted}
+        withPicture
+      />
+    </QuestionCard>
+  );
+};
