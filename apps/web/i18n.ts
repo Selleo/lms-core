b456f6d167375471e049951d03130222b75f5695
mateuszszczecina <m.szczecina@selleo.com>
2025-01-10 11:10:54 +0100

    fix: quick fix

--- apps/web/i18n.ts
+++ apps/web/i18n.ts
@@ -2,7 +2,7 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import enTranslations from "app/locales/en/translation.json";
-import plTranslations from 'app/locales/pl/translation.json'
+import plTranslations from 'app/locales/pl/translation.json';
 
 i18next.use(initReactI18next).init({
   fallbackLng: "en",
