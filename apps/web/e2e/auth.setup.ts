43e6dff91622883b3c8135578c3e10873568fc91
Jakub Wilk <jwilk436@gmail.com>
2025-01-21 16:46:42 +0100

    test: course settings e2e test

--- apps/web/e2e/auth.setup.ts
+++ apps/web/e2e/auth.setup.ts
@@ -3,10 +3,17 @@ import { test as setup } from "@playwright/test";
 import { AuthFixture } from "./fixture/auth.fixture";
 
 setup("authenticate", async ({ page }) => {
-  const authFixture = new AuthFixture(page);
-  await authFixture.login("student@example.com", "password");
+  const studentAuthFixture = new AuthFixture(page);
+  await studentAuthFixture.login("student@example.com", "password");
 
   await page.context().storageState({
     path: "e2e/.auth/user.json",
+  });
+
+  const adminAuthFixture = new AuthFixture(page);
+  await adminAuthFixture.login("admin@example.com", "password");
+
+  await page.context().storageState({
+    path: "e2e/.auth/admin.json",
   });
 });
