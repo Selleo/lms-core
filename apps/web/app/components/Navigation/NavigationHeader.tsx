f0fa5c8d5978ea48fbcb451d392361576a87f939
Piotr Pająk <piotrpajak.programista@gmail.com>
2025-01-31 15:36:52 +0100

    fix: correct logo display in the small sidebar

--- apps/web/app/components/Navigation/NavigationHeader.tsx
+++ apps/web/app/components/Navigation/NavigationHeader.tsx
@@ -15,7 +15,7 @@ export function NavigationHeader({ isMobileNavOpen, setIsMobileNavOpen }: Naviga
     <div className="flex w-full justify-between px-4 py-3 2xl:h-20 2xl:items-center 2xl:justify-center 2xl:p-0">
       <Link to="/" aria-label="Go to homepage">
         <Icon name="AppLogo" className="h-10 w-full 2xl:sr-only 3xl:not-sr-only 3xl:h-12" />
-        <Icon name="AppSignet" className="sr-only 2xl:not-sr-only 3xl:sr-only" />
+        <Icon name="AppSignet" className="sr-only 2xl:not-sr-only 2xl:size-10 3xl:sr-only" />
       </Link>
       <MobileMenuToggle isMobileNavOpen={isMobileNavOpen} setIsMobileNavOpen={setIsMobileNavOpen} />
     </div>
